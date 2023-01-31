eddbe90734e4281cb7f3bb5cd5bee13a0cefee58
bwrobel <bwrobel@everpress.pl>
2023-01-31 23:07:58 +0100

    update

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Domain\Converters\FlxZipArchive;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -25,6 +26,12 @@ return function (App $app) {
     $app->get('/single-file-converter', function (Request $request, Response $response) use ($app) {
 
         $response = $app->getContainer()->get('view')->render($response, 'ConverterSingleFilePicker.phtml');
+        return $response;
+    });
+
+    $app->get('/zip-file-converter', function (Request $request, Response $response) use ($app) {
+
+        $response = $app->getContainer()->get('view')->render($response, 'ConverterZipPicker.phtml');
         return $response;
     });
 
@@ -119,4 +126,128 @@ return function (App $app) {
         }
     });
 
+    $app->post('/convert-to-zip-webp-download', function (Request $request, Response $response) use ($uploadFolder)
+    {
+        $it = new RecursiveDirectoryIterator(__DIR__ . DIRECTORY_SEPARATOR . $uploadFolder, RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new RecursiveIteratorIterator($it,
+            RecursiveIteratorIterator::CHILD_FIRST);
+        foreach($files as $file) {
+            if ($file->isDir()){
+                rmdir($file->getRealPath());
+            } else {
+                unlink($file->getRealPath());
+            }
+        }
+        if (isset($_FILES['file'])) {
+            $uploadedFile = $_FILES['file'];
+            if ($uploadedFile['error'] === 0) {
+                $file_name = $uploadedFile["name"];
+                $path = __DIR__ . DIRECTORY_SEPARATOR . $uploadFolder;
+                $target_path = __DIR__ . DIRECTORY_SEPARATOR . $uploadFolder . DIRECTORY_SEPARATOR . $file_name;
+                move_uploaded_file($uploadedFile["tmp_name"], $target_path);
+
+                $valueArray = explode('.', $uploadedFile['name']);
+                if (WebpConverter::checkArchiveFormat($valueArray[1])) {
+                    unlink($target_path);
+                    return WebpConverter::wrongFileFormatResponse($response);
+                }
+
+                $zip = new ZipArchive();
+                $zip->open($target_path);
+                $zip->extractTo($path);
+                $zip->close();
+
+                unlink($target_path);
+                $new_path = $path . DIRECTORY_SEPARATOR . $valueArray[0];
+
+                $files = scandir($new_path);
+
+
+                foreach ($files as $file){
+                    if(WebpConverter::checkFileFormat(explode('.', $file)[1]) === false){
+                        $images[] = $file;
+                    }
+                }
+
+                foreach ($images as $image){
+                    $imageArray = explode('.', $image);
+                    $new_target_path = $new_path . DIRECTORY_SEPARATOR . $image;
+                    if ($imageArray[1] === 'png') {
+                        $img = WebpConverter::createPng($new_target_path);
+                    } elseif ($imageArray[1] === 'jpeg' || $imageArray[1] === 'jpg') {
+                        $img = WebpConverter::createJpg($new_target_path);
+                    }
+                    WebpConverter::toTrueColor($img);
+                    WebpConverter::convertImage($img, $new_path . DIRECTORY_SEPARATOR . $imageArray[0] . '.webp');
+                    imagedestroy($img);
+                    unlink($new_target_path);
+                }
+
+                $za = new FlxZipArchive();
+                $res = $za->open($new_path . '.zip', ZipArchive::CREATE);
+                if($res === TRUE)
+                {
+                    $za->addDir($new_path, basename($new_path));
+                    $za->close();
+                }
+
+                $it = new RecursiveDirectoryIterator($new_path, RecursiveDirectoryIterator::SKIP_DOTS);
+                $files = new RecursiveIteratorIterator($it,
+                    RecursiveIteratorIterator::CHILD_FIRST);
+                foreach($files as $file) {
+                    if ($file->isDir()){
+                        rmdir($file->getRealPath());
+                    } else {
+                        unlink($file->getRealPath());
+                    }
+                }
+                rmdir($new_path);
+                //unlink($target_path);
+//                $fh = fopen($new_path . '.zip', 'rb');
+//
+//                $stream = new Stream($fh); // create a stream instance for the response body
+
+//                $response = $response
+//                    ->withHeader('Content-Type', 'application/force-download')
+//                    ->withHeader('Content-Description', 'File Transfer')
+//                    ->withHeader('Content-length', filesize($new_path . '.zip'))
+//                    ->withHeader('Content-Type', 'application/octet-stream')
+//                    ->withHeader('Content-Disposition', 'attachment; filename="' . basename($new_path . '.zip') . '"')
+//                    ->withHeader('Expires', '0')
+//                    ->withHeader('Pragma', 'no-cache')
+//                    ->withBody($stream);
+                header('Content-Description: File Transfer');
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment;filename="'.basename($new_path . '.zip').'"');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($new_path . '.zip'));
+
+                ob_clean();
+                flush();
+                readfile($new_path . '.zip');
+                exit;
+
+
+//                return $response->withHeader('Content-Type', 'application/force-download')
+//                    ->withHeader('Content-Type', 'application/octet-stream')
+//                    ->withHeader('Content-Type', 'application/download')
+//                    ->withHeader('Content-Type', 'application/zip')
+//                    ->withHeader('Content-length', filesize($new_path . '.zip'))
+//                    ->withHeader('Content-Description', 'File Transfer')
+//                    ->withHeader('Content-Transfer-Encoding', 'binary')
+//                    ->withHeader('Content-Disposition', 'attachment; filename="' . basename($new_path . '.zip') . '"')
+//                    ->withHeader('Expires', '0')
+//                    ->withHeader('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
+//                    ->withHeader('Pragma', 'public')
+//                    ->withBody($stream);
+                //return $response;
+            } else {
+                return WebpConverter::uploadingFileFailedResponse($response);
+            }
+        } else {
+            return WebpConverter::missingFileResponse($response);
+        }
+    });
 };
--- src/Domain/Converters/WebpConverter.php
+++ src/Domain/Converters/WebpConverter.php
@@ -4,6 +4,7 @@ namespace App\Domain\Converters;
 
 use App\Domain\Converters\IConverter;
 use Psr\Http\Message\ResponseInterface as Response;
+use ZipArchive;
 
 class WebpConverter extends Converter implements IConverter
 {
@@ -12,10 +13,20 @@ class WebpConverter extends Converter implements IConverter
     use TImage;
 
     public static array $supportedFormats = ['png', 'jpg', 'jpeg'];
+    public static array $supportedArchiveFormats = ['zip', 'rar', '7z'];
 
     public static function checkFileFormat($format): bool
     {
-        if (!in_array($format, WebpConverter::$supportedFormats)) {
+        if (!in_array($format, self::$supportedFormats)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function checkArchiveFormat($format): bool
+    {
+        if (!in_array($format, self::$supportedArchiveFormats)) {
             return true;
         } else {
             return false;
@@ -53,5 +64,50 @@ class WebpConverter extends Converter implements IConverter
         $model = new ResponseModel(status: 400, error: 'File upload failed');
         $response->getBody()->write($model->jsonSerialize());
         return $response;
+    }
+
+    private static function createZip($files = array(), $dest = '', $overwrite = false) {
+        if (file_exists($dest) && !$overwrite) {
+            return false;
+        }
+        if (($files)) {
+            $zip = new ZipArchive();
+            if ($zip->open($dest, $overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
+                return false;
+            }
+            foreach ($files as $file) {
+                $zip->addFile($file, $file);
+            }
+            $zip->close();
+            return file_exists($dest);
+        } else {
+            return false;
+        }
+    }
+
+    public static function addZip($source, $destination) {
+        $files_to_zip = glob($source . '/*');
+        self::createZip($files_to_zip, $destination);
+    }
+}
+
+class FlxZipArchive extends ZipArchive
+{
+    public function addDir($location, $name)
+    {
+        $this->addEmptyDir($name);
+        $this->addDirDo($location, $name);
+    }
+    private function addDirDo($location, $name)
+    {
+        $name .= '/';
+        $location .= '/';
+        $dir = opendir ($location);
+        while ($file = readdir($dir))
+        {
+            if ($file == '.' || $file == '..') continue;
+            $do = (filetype( $location . $file) == 'dir') ? 'addDir' : 'addFile';
+            $this->$do($location . $file, $name . $file);
+        }
     }
 }
